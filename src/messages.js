6dcb9bfb780362f9fda91a40ac222fc9cc772ef9
Vladimir Zhukov <vzh.inbox@gmail.com>
2017-06-13 13:58:05 +0600

    refactor: simplify messages

--- src/messages.js
+++ src/messages.js
@@ -1,23 +1,23 @@
 const msg = {};
 
-msg.type = 'Value `{VALUE}` of `{PROP}` must be instance of `{TYPE}`.';
-msg.required = '`{PROP}` is required.';
-msg.oneOf = 'Value `{VALUE}` of `{PROP}` is not valid enum value.';
-msg.email = 'Value `{VALUE}` of `{PROP}` is not valid email.';
-msg.equals = 'Value `{VALUE}` of `{PROP}` must be equals to {PROP2}.';
+msg.type = 'Value must be instance of `{TYPE}`.';
+msg.required = 'Value is required.';
+msg.oneOf = 'Value is not valid enum value.';
+msg.email = 'Value is not valid email.';
+msg.equals = 'Value must be equals to {PROP2}.';
 
 msg.Number = {};
-msg.Number.min = 'Value `{VALUE}` of `{PROP}` is less than minimum allowed value `{MIN}`.';
-msg.Number.max = 'Value `{VALUE}` of `{PROP}` is more than maximum allowed value `{MAX}`.';
+msg.Number.min = 'Value is less than minimum allowed value `{MIN}`.';
+msg.Number.max = 'Value is more than maximum allowed value `{MAX}`.';
 
 msg.String = {};
-msg.String.minLength = 'Value `{VALUE}` of `{PROP}` is shorter than the minimum allowed length `{MINLENGTH}`.';
-msg.String.maxLength = 'Value `{VALUE}` of `{PROP}` is longer than the maximum allowed length `{MAXLENGTH}`.';
-msg.String.match = 'Value `{VALUE}` of `{PROP}` do not match to regex.';
+msg.String.minLength = 'Value is shorter than the minimum allowed length `{MINLENGTH}`.';
+msg.String.maxLength = 'Value is longer than the maximum allowed length `{MAXLENGTH}`.';
+msg.String.match = 'Value do not match to regex.';
 
 msg.Date = {};
-msg.Date.min = 'Value `{VALUE}` of `{PROP}` is before minimum allowed value `{MIN}`.';
-msg.Date.max = 'Value `{VALUE}` of `{PROP}` is after maximum allowed value `{MAX}`.';
+msg.Date.min = 'Value is before minimum allowed value `{MIN}`.';
+msg.Date.max = 'Value is after maximum allowed value `{MAX}`.';
 
 
 export default msg;
